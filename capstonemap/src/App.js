e93d2843900f5901a654478c61a4161c20ac89d6
윤태형 <52120957+YunTaeng@users.noreply.github.com>
2024-05-25 17:16:19 +0900

    z

---
+++ capstonemap/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Mainpage from './mainpage';
+function App() {
+  return (
+    <div>
+      <Mainpage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ capstonemap/src/mainpage.js
@@ -0,0 +1,15 @@
+// Mainpage.js
+
+import React from 'react';
+import scvchanger from './scvchanger';
+
+const Mainpage = () => {
+    return (
+        <div className="Mainpage">
+            <scvchanger />
+        </div>
+    );
+};
+
+
+export default Mainpage;
