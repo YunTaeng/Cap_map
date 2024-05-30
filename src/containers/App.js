595dec229adf1a1496ce5751ff959af69de77fe9
YunTaeng <howard9166@naver.com>
2024-05-31 04:02:16 +0900

    MAY last update

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,7 @@ import Autocomplete from '../components/autocomplete';
 import logo from '../images/Logo.png';
 import axios from 'axios';
 import './App.css';
+import './LeftContainer.scss'
 
 const App = () => {
     const getCurrentTime = () => { //한국의 현재 시간 받아오기
@@ -14,9 +15,9 @@ const App = () => {
         return kstTime.toISOString().substring(11, 16);
     };
 
-    const [origin, setOrigin] = useState('');
-    const [destination, setDestination] = useState('');
-    const [departureTime, setDepartureTime] = useState(getCurrentTime);
+    const [start_point, setStartPoint] = useState('');
+    const [end_point, setEndPoint] = useState('');
+    const [start_time, setStartTime] = useState(getCurrentTime());
     const [nodeAddr, setNodeAddr] = useState([]);
     const [eta, setEta] = useState('');
     const [path, setPath] = useState([]);
@@ -29,28 +30,28 @@ const App = () => {
     const [mapKey, setMapKey] = useState(0);
 
     const handleSearch = () => {
-        if (origin && destination && departureTime) {
-
-            const start_point = origin;
-            const end_point = destination;
-            const start_time = departureTime;
-
-            // axios.post('http://34.47.71.145:5000/find_path', {
-            //     start_point,
-            //     end_point,
-            //     start_time
-            // })
+        if (start_point && end_point&&start_time) {
+            /*--------------------------------*/
+            // axios.post('http://34.47.71.145:5000/find_path', {start_point, end_point, start_time})
             // .then(response => {
-                // const { path, total_hours, total_minutes, total_seconds,eta } = response.data;
-                // setEta(eta);
-                // setPath(path);
-                // setTotalHours(total_hours);
-                // setTotalMinutes(total_minutes);
-                // setTotalSeconds(total_seconds);
-                const path = ['서울특별시청', '한남IC', '잠원IC', '반포IC', '서초IC', '양재IC', '금토JC', '대왕판교IC', '판교JC', '판교IC', '서울TG', '신갈JC', '마성IC', '서용인JC', '용인IC', '양지IC', '덕평IC', '호법JC', '이천IC', '여주JC', '감곡IC', '충주JC', '북충주IC', '중앙탑Hi', '충주IC', '괴산IC', '연풍IC', '문경새재IC', '점촌함창IC', '북상주IC', '상주IC', '낙동JC', '상주JC', '도개IC', '서군위IC', '군위JCT', '동군위IC', '신녕IC', '화산JCT', '동영천IC', '북안IC', '영천JC', '건천IC', '경주IC', '활천IC', '언양JC', '울산JCT', '문수IC', '울주JC', '청량IC', '온양IC', '장안IC', '기장IC', '해운대IC', '동부산IC', '부산광역시청']
-            //     return 
-                axios.post('http://34.47.71.145:4000/get-node-info', { nodeNames: path })
-            //})
+            //     const { path, total_hours, total_minutes, total_seconds,eta } = response.data;
+            //     setEta(eta);
+            //     setPath(path);
+            //     setTotalHours(total_hours);
+            //     setTotalMinutes(total_minutes);
+            //     setTotalSeconds(total_seconds);
+            //     return axios.post('http://34.47.71.145:4000/get-node-info', { nodeNames: path })
+            // })
+            /*--------------------------------*/
+            const { path, total_hours, total_minutes, total_seconds,eta } = {'path': ['서울특별시청', '한남IC', '잠원IC', '반포IC', '서초IC', '양재IC', '금토JC', '대왕판교IC', '판교JC', '판교IC', '서울TG', '신갈JC', '마성IC', '서용인JC', '용인IC', '양지IC', '덕평IC', '호법JC', '이천IC', '여주JC', '감곡IC', '충주JC', '북충주IC', '중앙탑Hi', '충주IC', '괴산IC', '연풍IC', '문경새재IC', '점촌함창IC', '북상주IC', '상주IC', '낙동JC', '상주JC', '도개IC', '서군위IC', '군위JCT', '동군위IC', '신녕IC', '화산JCT', '동영천IC', '북안IC', '영천JC', '건천IC', '경주IC', '활천IC', '언양JC', '울산JCT', '문수IC', '울주JC', '청량IC', '온양IC', '장안IC', '기장IC', '해운대IC', '동부산IC', '부산광역시청'], 'total_hours': 5, 'total_minutes': 55, 'total_seconds': 0, 'eta': '00시 36분'}
+            setEta(eta);
+            setPath(path);
+            setTotalHours(total_hours);
+            setTotalMinutes(total_minutes);
+            setTotalSeconds(total_seconds);
+            axios.post('http://34.47.71.145:4000/get-node-info', { nodeNames: path })
+            /*--------------------------------*/
+
             .then(response => {
                 setNodeAddr(response.data);
                 setMapKey(prevKey => prevKey + 1);
@@ -60,16 +61,16 @@ const App = () => {
                 console.log('Loading stopped due to error');
             });
         } else {
-            setErrorMessage('전부 입력하시고 검색눌러주세요');
+            setErrorMessage('전부 입력하고 검색을 눌러주세요');
             setShowError(true);
             setTimeout(() => {
                 setShowError(false);
-            }, 3000); // Hide error message after 3 seconds
+            }, 3000); // 3초 후 에러메세지 사라짐
         }
     };
 
     useEffect(() => {
-        setDepartureTime(getCurrentTime()); // Set current time when component mounts
+        setStartTime(getCurrentTime());
     }, []);
 
     return (
@@ -80,25 +81,25 @@ const App = () => {
                 </div>
                 <div className="SearchBox">
                     <Autocomplete
-                        placeholder="출발지를 입력하세요"
-                        onSelectOption={setOrigin}
-                        inputValue={origin}
-                        setInputValue={setOrigin}
+                        placeholder="출발지 입력"
+                        onSelectOption={(option) => setStartPoint(option)}
+                        inputValue={start_point}
+                        setInputValue={setStartPoint}
                     />
                     <Autocomplete
-                        placeholder="도착지를 입력하세요"
-                        onSelectOption={setDestination}
-                        inputValue={destination}
-                        setInputValue={setDestination}
+                        placeholder="도착지 입력"
+                        onSelectOption={(option) => setEndPoint(option)}
+                        inputValue={end_point}
+                        setInputValue={setEndPoint}
                     />
-                    <label htmlFor="appt-time">출발 시간 입력:</label>
+                    <label htmlFor="appt-time">출발 시간 입력 :</label>
                     <input
                         id="appt-time"
                         type="time"
-                        value={departureTime}
-                        onChange={(e) => setDepartureTime(e.target.value)}
+                        value={start_time}
+                        onChange={(e) => setStartTime(e.target.value)}
                     />
-                    <button className="SearchButton" onClick={handleSearch}>검색</button>
+                    <button class="button-74" onClick={handleSearch}>검색</button>
                     {showError && <div className="ErrorMessage">{errorMessage}</div>}
                 </div>
                 <LeftContainer 
--- src/containers/LeftContainer.js
+++ src/containers/LeftContainer.js
@@ -8,6 +8,47 @@ const LeftContainer = ({ nodeAddr, map, eta, totalHours, totalMinutes, totalSeco
         setShowRouteDetail(!showRouteDetail); // Toggle visibility
     };
 
+    function convert12H(a) {//HH시 MM분 -> 오전/오후 HH시 MM분
+        var time = a;
+        var getTime = time.substring(0, 2);
+        var intTime = parseInt(getTime);
+
+        if (intTime < 12 ) {
+            var str = '오전 ';
+        } else {
+            str = '오후 '; 
+        }
+        if (intTime === 12) {var cvHour = intTime;}
+        else {cvHour = intTime%12;}
+
+        var res = str + ('0' + cvHour).slice(-2) +"시"+ time.slice(-4);  
+        return res;
+    }
+
+    function check_next_day(ETA, totalHours, totalMinutes) {
+        // ETA를 시간과 분으로 분리
+        const ETA_hours = parseInt(ETA.substring(0, 2));
+        const ETA_minutes = parseInt(ETA.substring(4, 6));
+    
+        // ETA에서 total 시간과 분을 뺌
+        let remaining_hours = ETA_hours - totalHours;
+        let remaining_minutes = ETA_minutes - totalMinutes;
+    
+        // 분을 시간으로 환산하여 시간에 반영
+        if (remaining_minutes < 0) {
+            remaining_hours -= 1;
+            remaining_minutes += 60;
+        }
+    
+        // 결과 반환
+        if (remaining_hours < 0) {
+            return <h4>다음날</h4>;
+        } else {
+            return null;
+        }
+    }
+    
+
     const setCenter = (lat, lng) => {
         if (kakao && map) {
             const moveLatLon = new kakao.maps.LatLng(lat, lng);
@@ -16,12 +57,35 @@ const LeftContainer = ({ nodeAddr, map, eta, totalHours, totalMinutes, totalSeco
         }
     };
 
+    // nodeAddr와 eta가 존재할 때만 렌더링
+    if (!nodeAddr || !eta) return null;
+
     return (
         <div className="LeftContainer">
-            <h2 style={{ textAlign: "center" }}>예상 도착 시간은 {eta}입니다</h2>
-            <h4 style={{ textAlign: "center" }}>총 여행 시간: {totalHours}시간 {totalMinutes}분 {totalSeconds}초</h4>
+            <div style={{display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <div style={{ textAlign: "center" }}>
+                    {totalHours > 0 && (
+                        <h1 style={{ fontSize: "2em", display: "inline" }}>
+                            <span style={{ fontSize: "1.7em" }}>{totalHours}</span>시간{" "}
+                        </h1>
+                    )}
+                    {(totalMinutes > 0 || (totalHours === 0 && totalMinutes === 0)) && (
+                        <h1 style={{ fontSize: "2em", display: "inline" }}>
+                            <span style={{ fontSize: "1.7em" }}>{totalMinutes}</span>분{" "}
+                        </h1>
+                    )}
+                </div>
+                <div style={{height: "60px", borderLeft: "2px solid #ccc" ,margin: "0 25px"}}></div>
+                <div style={{ marginTop: "-30px"}}>
+                    {check_next_day(eta, totalHours, totalMinutes)}
+                </div>
+                
+                <div style={{marginTop: "25px", marginLeft: "-49px"}}>
+                    <h4>{convert12H(eta)} 도착</h4>
+                </div>
+            </div>
 
-            <button onClick={toggleRouteDetail} style={{ float: 'right', marginRight: '20px' }}>Show/hide</button>
+            <button className="button-6" onClick={toggleRouteDetail}>상세보기</button>
             {showRouteDetail && (
                 <div className='RouteDetailComponent'>
                     <h2>경로</h2>
