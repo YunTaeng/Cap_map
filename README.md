0c69cc0d2584ec87e5179a797be75a72cf1677c1
YunTaeng <howard9166@naver.com>
2024-06-03 04:48:20 +0900

    app.js update

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 ## 실행
 <img width="1280" alt="1111" src="https://github.com/YunTaeng/Cap_map/assets/52120957/f107f198-1047-4563-954b-a450508dc6c5">
 <img width="1280" alt="11" src="https://github.com/YunTaeng/Cap_map/assets/52120957/eb12650a-df12-42e5-9e1d-8e4488fea240">
-![ezgif-1-cdb0b643a3](https://github.com/YunTaeng/Cap_map/assets/52120957/68d35d5c-2773-4f49-accc-40dedf7b44f5)
+
 <p align="center">
 <img src="https://github.com/YunTaeng/Cap_map/assets/52120957/68d35d5c-2773-4f49-accc-40dedf7b44f5">
 </p>
 ## 구성
+![아키텍쳐](https://github.com/YunTaeng/Cap_map/assets/52120957/0d07637e-96ed-4110-ac43-d17f508fd003)
+
 ![Logo](https://github.com/YunTaeng/Cap_map/assets/52120957/fdddbf3b-1056-4b51-8dab-e4e76dbb46d9)
 ![ㅇㅅㅇ](https://github.com/YunTaeng/Cap_map/assets/52120957/ae1204c4-720a-44f6-aec3-84075c471f98)
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import MapContainer from './MapContainer';
 import LeftContainer from './LeftContainer';
 import Autocomplete from '../components/autocomplete';
-import logo from '../images/Logo.png';
+import logo from '../images/Logo.svg';
 import axios from 'axios';
 import './App.css';
 import './LeftContainer.css'
 import './loading.css'
+
 
 const App = () => {
     const getCurrentTime = () => { //한국의 현재 시간 받아오기
@@ -30,10 +31,11 @@ const App = () => {
     const [mapKey, setMapKey] = useState(0);
     const [loading, setLoading] = useState(false);
     /*-----------------------------------*/
-    // var places = new kakao.maps.services.Places();
-    //     const nodeName=['장지IC']
-    
     // const { kakao } = window;
+    // var places = new kakao.maps.services.Places();
+    //     const nodeName=['기장JC']
+    
+        
     //     var keywordresult = []; // 결과를 저장할 배열
 
     //     nodeName.forEach(function(keyword, index) {
@@ -41,17 +43,17 @@ const App = () => {
     //             return function(result, status) {
     //                 if (status === kakao.maps.services.Status.OK) { // 결과를 배열에 추가
     //                     keywordresult[index] = {
-    //                         name : keyword,
+    //                         name : result[0].place_name,
     //                         lat: result[0].y,
     //                         lng: result[0].x
     //                     };
     //                     keywordresult[index+1] = {
-    //                         name : keyword,
+    //                         name : result[1].place_name,
     //                         lat: result[1].y,
     //                         lng: result[1].x
     //                     };
     //                     keywordresult[index+2] = {
-    //                         name : keyword,
+    //                         name : result[2].place_name,
     //                         lat: result[2].y,
     //                         lng: result[2].x
     //                     };
@@ -63,41 +65,40 @@ const App = () => {
     //     console.log(keywordresult)
 /*-----------------------------------*/
     const handleSearch = () => {  
-        if (start_point === end_point) {
+        if (start_point === end_point) {//출-도착지가 같을 경우
             setErrorMessage('출발지와 목적지를 다르게 설정하세요');
             setShowError(true);
             setTimeout(() => {
                 setShowError(false);
-            }, 3000); // 3초 후 에러메세지 사라짐
+            }, 3000);
             return;
         }
 
         if (start_point && end_point&&start_time) {
-            setLoading(true); // Show loading spinner
+            setLoading(true);
             const reqTime = new Date(); // 시작시각 기록
-            /*--------------------------------*/
-            axios.post('http://34.47.71.145:5000/find_path', {start_point, end_point, start_time})
-            .then(response => {
-                const resTime = new Date(); // 끝시간 기록
-                const timeDiff = resTime - reqTime; //
-                console.log(`경과한 시간: ${timeDiff} ms`);
-                const { path, total_hours, total_minutes, total_seconds,eta } = response.data;
-                setEta(eta);
-                setTotalHours(total_hours);
-                setTotalMinutes(total_minutes);
-                setTotalSeconds(total_seconds);
+        /*------------------------------------------------------------*/
+            // axios.post('http://34.47.71.145:5000/find_path', {start_point, end_point, start_time})
+            // .then(response => {
+            //     const resTime = new Date(); // 끝시간 기록
+            //     const timeDiff = resTime - reqTime; //
+            //     console.log(`경과한 시간: ${timeDiff} ms`);
+            //     const { path, total_hours, total_minutes, total_seconds,eta } = response.data;
+            //     setEta(eta);
+            //     setTotalHours(total_hours);
+            //     setTotalMinutes(total_minutes);
+            //     setTotalSeconds(total_seconds);
 
-                return axios.post('http://34.47.71.145:5000/get-node-info', { nodeNames: path })
-            })
-            /*--------------------------------*/
-            // const { path, total_hours, total_minutes, total_seconds,eta } = {'path': ['서울특별시청', '한남IC', '잠원IC', '반포IC', '서초IC', '양재IC', '금토JC', '대왕판교IC', '판교JC', '판교IC', '서울TG', '신갈JC', '마성IC', '서용인JC', '용인IC', '양지IC', '덕평IC', '호법JC', '이천IC', '여주JC', '감곡IC', '충주JC', '북충주IC', '중앙탑Hi', '충주IC', '괴산IC', '연풍IC', '문경새재IC', '점촌함창IC', '북상주IC', '상주IC', '낙동JC', '상주JC', '도개IC', '서군위IC', '군위JCT', '동군위IC', '신녕IC', '화산JCT', '동영천IC', '북안IC', '영천JC', '건천IC', '경주IC', '활천IC', '언양JC', '울산JCT', '문수IC', '울주JC', '청량IC', '온양IC', '장안IC', '기장IC', '해운대IC', '동부산IC', '부산광역시청'], 'total_hours': 0, 'total_minutes': 44, 'total_seconds': 0, 'eta': '16시 44분'}
-            // setEta(eta);
-            // setPath(path);
-            // setTotalHours(total_hours);
-            // setTotalMinutes(total_minutes);
-            // setTotalSeconds(total_seconds);
-            // axios.post('http://34.47.71.145:5000/get-node-info', { nodeNames: path })
-            /*--------------------------------*/
+            //     return axios.post('http://34.47.71.145:5000/get-node-info', { nodeNames: path })
+            // })
+        /*--------------------------------------------------------------*/
+            const { path, total_hours, total_minutes, total_seconds,eta } = {'path': ['서울특별시청', '한남IC', '잠원IC', '반포IC', '서초IC', '양재IC', '금토JC', '대왕판교IC', '판교JC', '판교IC', '서울TG', '신갈JC', '마성IC', '서용인JC', '용인IC', '양지IC', '덕평IC', '호법JC', '이천IC', '여주JC', '감곡IC', '충주JC', '북충주IC', '중앙탑Hi', '충주IC', '괴산IC', '연풍IC', '문경새재IC', '점촌함창IC', '북상주IC', '상주IC', '낙동JC', '상주JC', '도개IC', '서군위IC', '군위JCT', '동군위IC', '신녕IC', '화산JCT', '동영천IC', '북안IC', '영천JC', '건천IC', '경주IC', '활천IC', '언양JC', '울산JCT', '문수IC', '울주JC', '청량IC', '온양IC', '장안IC', '기장IC', '해운대IC', '동부산IC', '부산광역시청'], 'total_hours': 4, 'total_minutes': 44, 'total_seconds': 0, 'eta': '16시 44분'}
+            setEta(eta);
+            setTotalHours(total_hours);
+            setTotalMinutes(total_minutes);
+            setTotalSeconds(total_seconds);
+            axios.post('http://34.47.71.145:5000/get-node-info', { nodeNames: path })
+        /*--------------------------------------------------------------*/
 
             .then(response => {
                 setNodeAddr(response.data);
